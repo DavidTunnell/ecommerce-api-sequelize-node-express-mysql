34dfcc51216fd227dbf72f32a36b85fe84020895
David Tunnell <davidtunnell9@gmail.com>
2021-08-28 19:31:32 -0500

    now using sequalize

--- models/Category.js
+++ models/Category.js
@@ -4,17 +4,33 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
-Category.init(
-  {
+Category.init({
     // define columns
-  },
-  {
+    title: {
+        type: DataTypes.STRING
+    },
+    author: {
+        type: DataTypes.STRING
+    },
+    isbn: {
+        type: DataTypes.STRING
+    },
+    pages: {
+        type: DataTypes.INTEGER
+    },
+    edition: {
+        type: DataTypes.INTEGER
+    },
+    // Will become `is_paperback` in table due to `underscored` flag
+    isPaperback: {
+        type: DataTypes.BOOLEAN
+    }
+}, {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
-);
+});
 
 module.exports = Category;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
+const Category = require('./models/Category');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// Force true to drop/recreate table(s) on every sync
+sequelize.sync({ force: true }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
 });
